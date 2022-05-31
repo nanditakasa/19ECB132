e782a2c3750015d68e4ede88e316407b2b8d04ad
Nandita <91262484+nanditakasa@users.noreply.github.com>
2022-05-31 19:37:37 +0530

    Update insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -1,23 +1,35 @@
-# include <stdio.h>
-int main(void)
-{
-  int input[10] = {10, 9, 8, 7, 6, 5, 1, 2, 3, 4};
-  // Elements are hard coded
-
-  for (int i=1; i<10; i++)
-  {
-    int element = input[i];
-    int j = i-1;
-    while ( j >= 0 && input[j] > element) 
-    {
-      input[j+1] = input[j];
-      j--;
-    }
-    input[j+1] = element;
-  }
-
-  printf("\nElements after sorting are ...\n");
-  for(int i=0; i<10; i++)
-    printf("%d\t", input[i]);
-  return 0;  
-}
+#include <stdio.h>  
+void insert(int a[], int n) 
+{  
+    int i, j, temp;  
+    for (i = 1; i < n; i++) 
+    {  
+        temp = a[i];  
+        j = i - 1;  
+        while(j>=0 && temp <= a[j])
+        {    
+            a[j+1] = a[j];     
+            j = j-1;    
+        }    
+        a[j+1] = temp;    
+    }  
+}  
+  
+void printArr(int a[], int n)  
+{  
+    int i;  
+    for (i = 0; i < n; i++)  
+        printf("%d ", a[i]);  
+}  
+  
+int main()  
+{  
+    int a[] = { 10, 11, 13, 4, 32, 24 };  
+    int n = sizeof(a) / sizeof(a[0]); 
+    printf("After sorting array elements are - \n");    
+    printArr(a, n);  
+  
+    return 0;  
+}    
+//OUTPUT:After sorting array elements are -
+12 31 25 8 32 17//
